a5cdf304b23d343f072e10daa67f10c3d114b39d
uzairmirza <53081780+uzairmirza90@users.noreply.github.com>
2022-04-28 14:20:51 +0500

    Added Material ui components

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,92 +1,25 @@
+import { Button, TextField } from '@mui/material'
 import React from 'react'
-import { PageHeader, Form, Input, Button, Checkbox } from 'antd';
-import '../login/login.css'
+import '../login/login.scss'
+
 
 const Login = () => {
 
-    const onFinish = (values) => {
-        console.log('Success:', values);
-    };
-
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
-
-
   return (
     <div className='login-page'>
-            <PageHeader
-                className="site-page-header"
-                title="Login"
-            />
 
-            <Form
-                size='large'
-                className='login-form'
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your email!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Button type="primary" htmlType="submit">
-                        Login
-                    </Button>
-                </Form.Item>
-            </Form>
+        <div className="home-header">
+          <h1 className="home-heading">Login</h1>
         </div>
+
+        <div className="line"></div>
+
+        <div className='login-form'>
+            <TextField id='outlined-basic' label='Email' variant='outlined' style={{width: '300px'}}/>
+            <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" style={{width: '300px'}}/>
+            <Button variant='contained' size='large'>Login</Button>
+        </div>
+    </div>
   )
 }
 
--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -1,117 +1,24 @@
 import React from 'react'
-import { PageHeader, Form, Input, Button, Checkbox } from 'antd';
-import '../signup/signup.css'
+import { TextField, Button } from '@mui/material';
+import '../signup/signup.scss'
 
 const SignUp = () => {
-
-    const onFinish = (values) => {
-        console.log('Success:', values);
-    };
-
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
-
-
     return (
-        <div className='signup-page'>
-            <PageHeader
-                className="site-page-header"
-                title="Sign Up"
-            />
+        <div className='login-page'>
 
-            <Form
-                size='large'
-                className='signup-form'
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your username!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your email!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                    label="Confirm Password"
-                    name="confirmpassword"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your confirm password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Button type="primary" htmlType="submit">
-                        Sign Up
-                    </Button>
-                </Form.Item>
-            </Form>
+        <div className="home-header">
+          <h1 className="home-heading">Sign Up</h1>
         </div>
+
+        <div className="line"></div>
+
+        <div className='login-form'>
+            <TextField id='outlined-basic' label='Username' variant='outlined' style={{width: '300px'}}/>
+            <TextField id='outlined-basic' label='Email' variant='outlined' style={{width: '300px'}}/>
+            <TextField id="outlined-password-input" label="Password" type="password" autoComplete="current-password" style={{width: '300px'}}/>
+            <Button variant='contained' size='large'>Sign Up</Button>
+        </div>
+    </div>
     )
 }
 
